5f2b5a6bf54e2ec7c2b1bde23587a25df8389c78
Aryan Khurana <aryankhurana1511@gmail.com>
2024-12-27 06:08:15 -0500

    test: CD 02

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
+    url: 'https://AryanK1511.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     // GitHub pages deployment configuration
